5fa3389322d1622871d212fdba484d9e9e26bcfd
Nick Mealey <mealey@partstash.com>
2014-11-03 11:11:15 -0800

    Forms will now save storage on unload.

--- mealey-zortman/forms/forms.js
+++ mealey-zortman/forms/forms.js
@@ -1,14 +1,22 @@
 $(document).ready(function(){
-  $('input').each(function(){
+  
+  // Get all fields
+  var fields = $('input');
+  
+  // Set any previous data
+  fields.each(function(){
+    console.log($(this).attr('id'));
     $(this).val(localStorage[$(this).attr('id')]);
-  }).blur(function(){
-    console.log($(this));
-    localStorage.setItem($(this).attr('id'), $(this).val());
   });
+  
+  // Before we leave, let's set some data
+  $(window).unload(function(){
+    fields.each(function(){
+      localStorage.setItem($(this).attr('id'), $(this).val());
+    })
+  });
+  
   document.getElementById("clear-button").onclick=function(){
     localStorage.clear();
   };
-
 });
-
-
