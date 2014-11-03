8ee8decb556a2a4eab91a409a997620f32bbd4ea
Levi Zortman <levizortman@gmail.com>
2014-11-03 11:33:55 -0800

    replace address text area with separate inputs

--- mealey-zortman/forms/forms.js
+++ mealey-zortman/forms/forms.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
-  
+
   // Get all fields
   var fields = $('input, textarea, select');
-  
+
   // Set any previous data
   fields.each(function(){
     $(this).val(localStorage[$(this).attr('id')]);
   });
-  
+
   // Before we leave, let's set some data
   $(window).unload(function(){
     fields.each(function(){
       localStorage.setItem($(this).attr('id'), $(this).val());
     })
   });
-  
+
   document.getElementById("clear-button").onclick=function(){
     localStorage.clear();
   };
 });
+
