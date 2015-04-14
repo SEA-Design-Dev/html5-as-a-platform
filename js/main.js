08d1a012f63b8b16cfc53ba2aac07f462a59e412
DonQuality <santosemmanuel@outlook.com>
2015-04-14 10:17:14 -0700

    jquery to js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,40 @@
-$('#show-R-Form').on('click', function() {
-  $('#register-form').removeClass('hidden');
-});
+// on click show form
 
-$('#show-R-Form').on('click', function(){
-  $(this).fadeOut();
-});
+var form = document.getElementById('register-form');
+var registerBtn = document.getElementById('showForm');
+function showForm(){
+  form.style.visibility='visible';
+}
+registerBtn.addEventListener('click', showForm);
 
-$('#hide-form').on('click', function(e){
-  $('#register-form').fadeOut();
-  e.preventDefault();
-});
+// $('#hide-form').on('click', function(e){
+//   $('#register-form').fadeOut();
+//   e.preventDefault();
+// });
 
-$('#show-drop-menu').on('click', function(){
-  $('#drop-menu').slideToggle();
-});
+// hide form
+
+var exitBtn=document.getElementById('hide-form');
+function hideForm(){
+  form.style.visibility='hidden';
+}
+exitBtn.addEventListener('click', hideForm);
+
+// $('#show-drop-menu').on('click', function(){
+//   $('#drop-menu').slideToggle();
+// });
+
+// on click show drop menu
+
+var dropMenu = document.getElementById('dropMenu');
+var showDropMenu = document.getElementById('showDropMenu');
+function dropDown(){
+  if (dropMenu.style.display=='block'){
+    dropMenu.style.display='none'
+  } else{
+    dropMenu.style.display='block'
+  }
+}
+
+showDropMenu.addEventListener('click', dropDown);
+
